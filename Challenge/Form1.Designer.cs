ef38cffbad8855e9e778278fc5f3e0d63e788a3a
Darussalaam-Nur-Rasyidu <darussalaamnurrasyidu@gmail.com>
2021-10-06 17:34:44 +0700

    Add project files.

---
+++ Challenge/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace Challenge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbBoxOperation = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNilaiA = new System.Windows.Forms.TextBox();
+            this.txtNilaiB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtHasil = new System.Windows.Forms.TextBox();
+            this.btnHasil = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbBoxOperation
+            // 
+            this.cmbBoxOperation.FormattingEnabled = true;
+            this.cmbBoxOperation.Items.AddRange(new object[] {
+            "Penambahan",
+            "Pengurangan",
+            "Perkalian",
+            "Pembagian",
+            "Pangkat",
+            "Modulo"});
+            this.cmbBoxOperation.Location = new System.Drawing.Point(61, 9);
+            this.cmbBoxOperation.Name = "cmbBoxOperation";
+            this.cmbBoxOperation.Size = new System.Drawing.Size(121, 21);
+            this.cmbBoxOperation.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Operasi";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nilai A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nilai B";
+            // 
+            // txtNilaiA
+            // 
+            this.txtNilaiA.Location = new System.Drawing.Point(61, 62);
+            this.txtNilaiA.Name = "txtNilaiA";
+            this.txtNilaiA.Size = new System.Drawing.Size(121, 20);
+            this.txtNilaiA.TabIndex = 4;
+            // 
+            // txtNilaiB
+            // 
+            this.txtNilaiB.Location = new System.Drawing.Point(61, 94);
+            this.txtNilaiB.Name = "txtNilaiB";
+            this.txtNilaiB.Size = new System.Drawing.Size(121, 20);
+            this.txtNilaiB.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Hasil";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtHasil
+            // 
+            this.txtHasil.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtHasil.Location = new System.Drawing.Point(61, 136);
+            this.txtHasil.Name = "txtHasil";
+            this.txtHasil.Size = new System.Drawing.Size(121, 20);
+            this.txtHasil.TabIndex = 7;
+            // 
+            // btnHasil
+            // 
+            this.btnHasil.Location = new System.Drawing.Point(107, 183);
+            this.btnHasil.Name = "btnHasil";
+            this.btnHasil.Size = new System.Drawing.Size(75, 23);
+            this.btnHasil.TabIndex = 8;
+            this.btnHasil.Text = "Hitung";
+            this.btnHasil.UseVisualStyleBackColor = true;
+            this.btnHasil.Click += new System.EventHandler(this.btnHasil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(260, 282);
+            this.Controls.Add(this.btnHasil);
+            this.Controls.Add(this.txtHasil);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNilaiB);
+            this.Controls.Add(this.txtNilaiA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbBoxOperation);
+            this.Name = "Form1";
+            this.Text = "Aplikasi Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbBoxOperation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNilaiA;
+        private System.Windows.Forms.TextBox txtNilaiB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtHasil;
+        private System.Windows.Forms.Button btnHasil;
+    }
+}
+
---
+++ Challenge/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculatorMachine;
+
+namespace Challenge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHasil_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(txtNilaiA.Text);
+            int b = int.Parse(txtNilaiB.Text);
+
+            var cal = new Calculator();
+            int idx = cmbBoxOperation.SelectedIndex;
+            txtHasil.Clear();
+            if (idx == 0) txtHasil.AppendText(cal.Penambahan(a, b).ToString());
+            if (idx == 1) txtHasil.AppendText(cal.Pengurangan(a, b).ToString());
+            if (idx == 2) txtHasil.AppendText(cal.Perkalian(a, b).ToString());
+            if (idx == 3) txtHasil.AppendText(Calculator.Pembagian(a, b).ToString());
+            if (idx == 4) txtHasil.AppendText(cal.Pangkat(a, b).ToString());
+            if (idx == 5) txtHasil.AppendText(Calculator.Modulo(a, b).ToString());
+        }
+    }
+}
